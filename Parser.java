ce07e0288678405746f270d366e61748f64cb5d7
Dr Royal Thomas <royal.thomas@ucdconnect.ie>
2019-02-27 22:15:25 +0000

    Parser implementation using JavaParser

---
+++ Parser.java
@@ -0,0 +1,55 @@
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseResult;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.comments.Comment;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * JavaDoc Comment
+ */
+public class Parser {
+    // This is a comment
+
+    private static class MethodVisitor extends VoidVisitorAdapter {
+
+        @Override
+        public void visit(MethodDeclaration n, Object arg) {
+            System.out.println("Found Method - " +n.getName());
+            if(n.getName().asString().equals("visit")){
+                System.out.println("Printing out method definition of method - 'visit'");
+                System.out.println(n.getBody());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String filePath = "src/main/java/Parser.java";
+
+        // This is also a comment
+        try {
+            CompilationUnit xd = JavaParser.parse(new File(filePath));
+            List<Comment> comments = xd.getComments();
+            new MethodVisitor().visit(xd, null);
+            System.out.println("\n\nFound (" + comments.size() + ") Comments");
+            comments.forEach((comment ->
+                    System.out.println(comment.getContent())
+            ));
+
+
+        } catch (Exception ex) {
+            /**
+             * Multiline comment
+             */
+            // Single line comment hmm
+            System.err.println(ex);
+            System.exit(1);
+        }
+
+        System.exit(0);
+    }
+
+}
