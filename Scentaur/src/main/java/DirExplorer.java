c695c603a890c1782eac1453ec3d6325dd4f330e
Gajun Young <ga.young@ucdconnect.ie>
2019-03-16 19:13:11 +0000

    Added packages

---
+++ Scentaur/src/main/java/DirExplorer.java
@@ -0,0 +1,27 @@
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.*;
+
+import com.github.javaparser.JavaParser;
+
+public class DirExplorer {
+	/*String directory;
+	public DirExplorer(String directory) {
+		this.directory = directory;
+	}
+	
+	public ArrayList<String> lookupFiles(){
+		List<File> files = (List<File>) FileUtils.listFiles(directory,".java", true);
+		ArrayList parsedFiles<ParsedFile>;
+		for(File file: files) {
+			parsedFiles.add(new JavaParser(file.getCanonicalPath()));
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("TEST");
+	}*/
+}
