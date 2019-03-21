ae6a1e3f07af4114c5e49d9649baf8380ab5d282
Gajun Young <ga.young@ucdconnect.ie>
2019-03-21 17:28:10 +0000

    More info added to longparameterList

--- Scentaur/src/main/java/smell/bloater/LongParameterList.java
+++ Scentaur/src/main/java/smell/bloater/LongParameterList.java
@@ -5,27 +5,44 @@ import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LongParameterList extends BloatersDetectors {
+public class LongParameterList extends VoidVisitorAdapter<Void> implements Bloatable {
 
+	public static final int PARAMETER_THRESHOLD = 3;
     private List<MethodDeclaration> longParameterMethods;
-
+   
     public LongParameterList(){
-    	setThreshold(5);
         longParameterMethods = new ArrayList<>();
     }
 
+    /**
+     * @return a list of MethodDeclarations that has 3 or more parameters
+     */
     public List<MethodDeclaration> get(){
         return longParameterMethods;
     }
 
+    /**
+     * Visits each compilation unit, checking its method parameter is longer than 2
+     */
     @Override
     public void visit(MethodDeclaration n, Void args){
         int numParams = n.getParameters().size();
-        if (numParams > super.threshold) {
+        
+        if (numParams >= PARAMETER_THRESHOLD) {
             longParameterMethods.add(n);
         }
         
-        System.out.println(n.getParentNode().get());
+        System.out.println(n.getParentNode().get().findCompilationUnit().get().getStorage().get().getPath());
     }
+
+	public boolean isClassEmpty(Object obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void setThreshold(int value) {
+		// TODO Auto-generated method stub
+		
+	}
     
 }
