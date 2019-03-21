d9dff520acf442bc82404a69eb07f541f2bffa33
Dr Royal Thomas <royal.thomas@ucdconnect.ie>
2019-03-21 20:00:14 +0000

    Made issues into a package

--- Scentaur/src/main/java/Main.java
+++ Scentaur/src/main/java/Main.java
@@ -5,13 +5,14 @@ import java.util.List;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.MethodDeclaration;
 
+import smell.Issue;
 import smell.bloater.LongMethodDetector;
 import smell.bloater.LongParameterList;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		Parser parse = new Parser(new File("..\\testProject\\src").getAbsolutePath());
+		Parser parse = new Parser(new File("..//testProject//src").getAbsolutePath());
 		List<CompilationUnit> all = parse.getAllCu();
 
 		LongParameterList longParameterList = new LongParameterList();
@@ -21,7 +22,10 @@ public class Main {
 		all.forEach(c -> {
 			
 			//System.out.println(c.getStorage().get().getPath());	//Test obtains all java file path
-			c.accept(longParameterList, null);
+			Issue getIssue = c.accept(longParameterList, null);
+			if(getIssue != null){
+				System.out.println("Found an issue(" +getIssue.getIssueType()+") with method " + getIssue.getMethodName() + " within class " + getIssue.getClassName() + " on line number:" + getIssue.getLineNumber());
+			}
 			c.accept(longMethod, null);
 		});
 		
