f0180b4f7b152a318bac5a0b109fa4c49f39decc
Dr Royal Thomas <royal.thomas@ucdconnect.ie>
2019-03-21 20:01:15 +0000

    Returns issue object now

--- Scentaur/src/main/java/smell/bloater/LongParameterList.java
+++ Scentaur/src/main/java/smell/bloater/LongParameterList.java
@@ -1,48 +1,58 @@
 package smell.bloater;
+
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.comments.Comment;
+import com.github.javaparser.ast.visitor.GenericVisitorAdapter;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class LongParameterList extends VoidVisitorAdapter<Void> {
+import smell.Issue;
 
-	public static final int PARAMETER_THRESHOLD = 3;
+public class LongParameterList extends GenericVisitorAdapter<Issue, Void> {
+
+    public static final int PARAMETER_THRESHOLD = 3;
     private List<MethodDeclaration> longParameterMethods;
-   
-    public LongParameterList(){
+
+    public LongParameterList() {
         longParameterMethods = new ArrayList<>();
     }
 
     /**
      * @return a list of MethodDeclarations that has 3 or more parameters
      */
-    public List<MethodDeclaration> get(){
+    public List<MethodDeclaration> get() {
         return longParameterMethods;
     }
 
     /**
      * Visits each compilation unit, checking its method parameter is longer than 2
      */
-    @Override
-    public void visit(MethodDeclaration n, Void args){
+
+    public Issue visit(MethodDeclaration n, Void args) {
         int numParams = n.getParameters().size();
-        
+
         if (numParams >= PARAMETER_THRESHOLD) {
             longParameterMethods.add(n);
-            addComment(n);   
+            addComment(n);
+
+            return new Issue(n.getParentNode().get().findCompilationUnit().get().getClass().getSimpleName(), n.getName().asString(), n.getBegin().get().line,
+                    "Long Parameter List", "Has too many parameters in method declaration");
+
         }
 
+
+        return null;
     }
-    
+
     /**
      * Add a comment above a method to indicate that its a smell
-     * @param n
      */
     private void addComment(MethodDeclaration n) {
-    	Comment comment = n.getComment().get();
-    	n.setJavadocComment(comment.getContent() + "\n\nSmellDetected: " + this.getClass().getSimpleName());
+        Comment comment = n.getComment().get();
+        n.setJavadocComment(
+                comment.getContent() + "\n\nSmellDetected: " + this.getClass().getSimpleName());
     }
-    
+
 }
