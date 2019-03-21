57e64acb78b209a2796e5c8171f7298592b2a30b
Dr Royal Thomas <royal.thomas@ucdconnect.ie>
2019-03-21 20:13:41 +0000

    Renamed Issue

---
+++ Scentaur/src/main/java/smell/Smell.java
@@ -0,0 +1,41 @@
+package smell;
+
+public class Smell {
+
+    private String className;
+    private int lineNumber;
+    private String methodName;
+    private String issueType;
+    private String comment;
+
+    public Smell(String className, String methodName, int lineNumber, String issueType,
+            String comment) {
+        this.className = className;
+        this.lineNumber = lineNumber;
+        this.issueType = issueType;
+        this.methodName = methodName;
+        this.comment = comment;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public int getLineNumber() {
+        return lineNumber;
+    }
+
+    public String getIssueType() {
+        return issueType;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+
+}
