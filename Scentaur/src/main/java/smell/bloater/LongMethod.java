94df6ff10438d46fc8a2aeb3a49f7c9c84588b61
Bill-IN <will2@outlook.ie>
2019-03-30 18:32:16 +0000

    Added Javadoc to explain visit method in LongMethod class

--- Scentaur/src/main/java/smell/bloater/LongMethod.java
+++ Scentaur/src/main/java/smell/bloater/LongMethod.java
@@ -1,8 +1,6 @@
 package smell.bloater;
 
 import java.util.ArrayList;
-import java.util.List;
-
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.MethodDeclaration;
 
@@ -15,6 +13,10 @@ public class LongMethod extends Bloater{
 		smell = new ArrayList<>();
 	}
 	
+	/**
+	 * Visits each MethodDeclaration for a Compilation Unit checking its 
+	 * body length is below the method length threshold.
+     */
 	public void visit(MethodDeclaration n, Void args) {
 		String[] lines = n.getBody().toString().split("\r\n|\r|\n");
 		if(lines.length >= METHOD_LENGTH_THRESHOLD) smell.add(n);
