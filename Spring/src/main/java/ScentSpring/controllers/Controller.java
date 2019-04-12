6d086c2746b93b4319822b53f054381321971594
Dr Royal Thomas <royal.thomas@ucdconnect.ie>
2019-04-12 13:54:55 +0100

    Defaults on port 80 and outputs html file

--- Spring/src/main/java/ScentSpring/controllers/Controller.java
+++ Spring/src/main/java/ScentSpring/controllers/Controller.java
@@ -1,12 +1,13 @@
 package ScentSpring.controllers;
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@org.springframework.stereotype.Controller
 public class Controller {
-    @GetMapping("/hello")
-    public String getHelloMessage(){
-        return "Hello, World!";
+    @RequestMapping("/")
+    public String greeting(){
+        return "index.html";
     }
 }
