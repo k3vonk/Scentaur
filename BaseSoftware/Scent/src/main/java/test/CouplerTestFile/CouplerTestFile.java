fad3c020eed3d8f0d0e3b8c9a0a5cb3a410559a9
Dr Royal Thomas <royal.thomas@ucdconnect.ie>
2019-05-02 13:42:31 +0100

    Added a few Tests.

---
+++ BaseSoftware/Scent/src/main/java/test/CouplerTestFile/CouplerTestFile.java
@@ -0,0 +1,13 @@
+package test.CouplerTestFile;
+
+import com.github.javaparser.ast.Node;
+
+import java.io.File;
+
+public class CouplerTestFile {
+
+    public void Test(){
+        File myFile = new File("src/main/java/test/CouplerTestFile");
+        myFile.getName().split("").clone().clone().clone();
+    }
+}
---
+++ BaseSoftware/Scent/src/main/java/test/DispensableTestFile/DataClassTest.java
@@ -0,0 +1,28 @@
+package test.DispensableTestFile;
+
+public class DataClassTest {
+    /**
+     *
+     *
+     *
+     *
+     *
+     */
+    int x;
+    int y;
+
+    //test
+    public DataClassTest(int x, int y, int z){
+        this.x = x;
+        this.y = y;
+    }
+
+    //teststststst
+    public void setX(int x){
+        this.x = x;
+    }
+    //sdfsdfsdfsdfsdfsdf
+    //testst
+    //test
+
+}
