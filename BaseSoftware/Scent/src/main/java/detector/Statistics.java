fd43d511c8f55dc0a45e9f1b763c18a7b1e456ca
Bill-IN <will2@outlook.ie>
2019-05-02 14:22:46 +0100

    Added stats to check no. times coupler and dispensable detected in entire project

--- BaseSoftware/Scent/src/main/java/detector/Statistics.java
+++ BaseSoftware/Scent/src/main/java/detector/Statistics.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import smell.Smell;
 import smell.Smell.Abusers;
 import smell.Smell.Bloaters;
+import smell.Smell.Couplers;
+import smell.Smell.Dispensables;
 import smell.abuser.Abuser;
 import smell.bloater.Bloater;
 import smell.coupler.Coupler;
@@ -66,6 +68,46 @@ public class Statistics {
 	}
 	
 	/**
+	 * Computes total number of times a Couper smell was detected in 
+	 * entire project.
+	 * 
+	 * @return numTimesCouplerDetected
+	 */
+	public int computeNumTimesCouplerDetectedInProject() {
+		int numTimesCouplerDetected = 0;
+		//Iterate over each type of OOPAbuser smell
+		for(Map<String, Map<Couplers, Smell>> map: couplers.getCouplers()) {
+			//Iterate over file names this OOPAbuser was detected
+			for(String fileName: map.keySet()) {	 
+				for(Couplers coupler: map.get(fileName).keySet()) {	
+					numTimesCouplerDetected += map.get(fileName).get(coupler).getIssue().size();
+				}
+			}
+		}
+		return numTimesCouplerDetected;
+	}
+	
+	/**
+	 * Computes total number of times a Dispensable smell was detected in 
+	 * entire project.
+	 * 
+	 * @return numTimesDispensableDetected
+	 */
+	public int computeNumTimesDispensableDetectedInProject() {
+		int numTimesDispensableDetected = 0;
+		//Iterate over each type of OOPAbuser smell
+		for(Map<String, Map<Dispensables, Smell>> map: dispensables.getDispensables()) {
+			//Iterate over file names this OOPAbuser was detected
+			for(String fileName: map.keySet()) {	 
+				for(Dispensables dispensable: map.get(fileName).keySet()) {	
+					numTimesDispensableDetected += map.get(fileName).get(dispensable).getIssue().size();
+				}
+			}
+		}
+		return numTimesDispensableDetected;
+	}
+	
+	/**
 	 * @return String representation of all computed statistics
 	 */
 	@Override
@@ -73,6 +115,8 @@ public class Statistics {
 		String statsStr = "____SMELL STATISTICS____\n\n";
 		statsStr += "Total number of times an OOPAbuser smell was detected in the entire project:  " + this.computeNumTimesOOPAbuserDetectedInProject() + "\n";
 		statsStr += "Total number of times a Bloater smell was detected in the entire project:  " + this.computeNumTimesBloaterDetectedInProject() + "\n";
+		statsStr += "Total number of times a Coupler smell was detected in the entire project:  " + this.computeNumTimesCouplerDetectedInProject() + "\n";
+		statsStr += "Total number of times a Dispensable smell was detected in the entire project:  " + this.computeNumTimesDispensableDetectedInProject() + "\n";
 		return statsStr;
 	}
 }
